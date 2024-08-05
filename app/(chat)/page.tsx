64f6d7a4d849f7641286f0025bc905a4a86f8826
Christopher David <chris@arcadelabs.co>
2024-08-05 10:25:43 -0500

    favicon

--- app/(chat)/page.tsx
+++ app/(chat)/page.tsx
@@ -6,17 +6,17 @@ import { Session } from '@/lib/types'
 import { getMissingKeys } from '@/app/actions'
 
 export const metadata = {
-  title: 'Next.js AI Chatbot'
+    title: 'Chat',
 }
 
 export default async function IndexPage() {
-  const id = nanoid()
-  const session = (await auth()) as Session
-  const missingKeys = await getMissingKeys()
+    const id = nanoid()
+    const session = (await auth()) as Session
+    const missingKeys = await getMissingKeys()
 
-  return (
-    <AI initialAIState={{ chatId: id, messages: [] }}>
-      <Chat id={id} session={session} missingKeys={missingKeys} />
-    </AI>
-  )
+    return (
+        <AI initialAIState={{ chatId: id, messages: [] }}>
+            <Chat id={id} session={session} missingKeys={missingKeys} />
+        </AI>
+    )
 }
