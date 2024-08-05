94140d969c1adec7b16f4cd75dadf5dc8f6438a9
Christopher David <chris@arcadelabs.co>
2024-08-05 17:29:36 -0500

    Fix sidebardesktop I think

--- components/sidebar-desktop.tsx
+++ components/sidebar-desktop.tsx
@@ -1,17 +1,19 @@
+"use client"
+
 import { Sidebar } from '@/components/sidebar'
-import { auth } from '@clerk/nextjs'
+import { useAuth } from '@clerk/nextjs'
 import { ChatHistory } from '@/components/chat-history'
 
-export async function SidebarDesktop() {
-  const { userId } = auth()
+export function SidebarDesktop() {
+    const { userId } = useAuth()
 
-  if (!userId) {
-    return null
-  }
+    if (!userId) {
+        return null
+    }
 
-  return (
-    <Sidebar className="peer absolute inset-y-0 z-30 hidden -translate-x-full border-r bg-muted duration-300 ease-in-out data-[state=open]:translate-x-0 lg:flex lg:w-[250px] xl:w-[300px]">
-      <ChatHistory userId={userId} />
-    </Sidebar>
-  )
+    return (
+        <Sidebar className="peer absolute inset-y-0 z-30 hidden -translate-x-full border-r bg-muted duration-300 ease-in-out data-[state=open]:translate-x-0 lg:flex lg:w-[250px] xl:w-[300px]">
+            <ChatHistory userId={userId} />
+        </Sidebar>
+    )
 }
