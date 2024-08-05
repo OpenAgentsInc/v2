6eb0bc47c4cf73fbc945b2a4fc712c891f2f7918
Christopher David <chris@arcadelabs.co>
2024-08-05 16:31:05 -0500

    models

---
+++ lib/models.ts
@@ -0,0 +1,5 @@
+export const models = [
+    { name: 'Claude 3.5 Sonnet', id: 'claude-3-5-sonnet-20240620', provider: 'anthropic' },
+    { name: 'Claude 3 Haiku', id: 'claude-3-haiku-20240307', provider: 'anthropic' },
+    { name: 'GPT-4o', id: 'gpt-4o', provider: 'openai' }
+]
