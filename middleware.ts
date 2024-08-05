fa35796750201fe92f13451da691e5c1a4c2195b
Christopher David <chris@arcadelabs.co>
2024-08-05 16:51:56 -0500

    ClerkProvider

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,12 @@
-import NextAuth from 'next-auth'
-import { authConfig } from './auth.config'
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default NextAuth(authConfig).auth
+export default clerkMiddleware();
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)']
-}
+    matcher: [
+        // Skip Next.js internals and all static files, unless found in search params
+        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+        // Always run for API routes
+        '/(api|trpc)(.*)',
+    ],
+};
