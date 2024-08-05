bc1b1624792357ad286fc47c9b49ad4cf4c9c7bf
Christopher David <chris@arcadelabs.co>
2024-08-05 10:22:26 -0500

    Add JetBrainsMono font

---
+++ lib/fonts.ts
@@ -0,0 +1,8 @@
+import { JetBrains_Mono } from "next/font/google"
+
+export const jetbrainsMono = JetBrains_Mono({
+    subsets: ["latin"],
+    display: "swap",
+    variable: "--font-jetbrains-mono",
+})
+
