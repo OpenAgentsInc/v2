390d79ff5c60aa30cb0dd7d93b233a536f84f2fa
Christopher David <chris@arcadelabs.co>
2024-08-05 10:29:36 -0500

    bg black

--- components/chat.tsx
+++ components/chat.tsx
@@ -13,72 +13,72 @@ import { useScrollAnchor } from '@/lib/hooks/use-scroll-anchor'
 import { toast } from 'sonner'
 
 export interface ChatProps extends React.ComponentProps<'div'> {
-  initialMessages?: Message[]
-  id?: string
-  session?: Session
-  missingKeys: string[]
+    initialMessages?: Message[]
+    id?: string
+    session?: Session
+    missingKeys: string[]
 }
 
 export function Chat({ id, className, session, missingKeys }: ChatProps) {
-  const router = useRouter()
-  const path = usePathname()
-  const [input, setInput] = useState('')
-  const [messages] = useUIState()
-  const [aiState] = useAIState()
+    const router = useRouter()
+    const path = usePathname()
+    const [input, setInput] = useState('')
+    const [messages] = useUIState()
+    const [aiState] = useAIState()
 
-  const [_, setNewChatId] = useLocalStorage('newChatId', id)
+    const [_, setNewChatId] = useLocalStorage('newChatId', id)
 
-  useEffect(() => {
-    if (session?.user) {
-      if (!path.includes('chat') && messages.length === 1) {
-        window.history.replaceState({}, '', `/chat/${id}`)
-      }
-    }
-  }, [id, path, session?.user, messages])
+    useEffect(() => {
+        if (session?.user) {
+            if (!path.includes('chat') && messages.length === 1) {
+                window.history.replaceState({}, '', `/chat/${id}`)
+            }
+        }
+    }, [id, path, session?.user, messages])
 
-  useEffect(() => {
-    const messagesLength = aiState.messages?.length
-    if (messagesLength === 2) {
-      router.refresh()
-    }
-  }, [aiState.messages, router])
+    useEffect(() => {
+        const messagesLength = aiState.messages?.length
+        if (messagesLength === 2) {
+            router.refresh()
+        }
+    }, [aiState.messages, router])
 
-  useEffect(() => {
-    setNewChatId(id)
-  })
-
-  useEffect(() => {
-    missingKeys.map(key => {
-      toast.error(`Missing ${key} environment variable!`)
+    useEffect(() => {
+        setNewChatId(id)
     })
-  }, [missingKeys])
 
-  const { messagesRef, scrollRef, visibilityRef, isAtBottom, scrollToBottom } =
-    useScrollAnchor()
+    useEffect(() => {
+        missingKeys.map(key => {
+            toast.error(`Missing ${key} environment variable!`)
+        })
+    }, [missingKeys])
 
-  return (
-    <div
-      className="group w-full overflow-auto pl-0 peer-[[data-state=open]]:lg:pl-[250px] peer-[[data-state=open]]:xl:pl-[300px]"
-      ref={scrollRef}
-    >
-      <div
-        className={cn('pb-[200px] pt-4 md:pt-10', className)}
-        ref={messagesRef}
-      >
-        {messages.length ? (
-          <ChatList messages={messages} isShared={false} session={session} />
-        ) : (
-          <EmptyScreen />
-        )}
-        <div className="w-full h-px" ref={visibilityRef} />
-      </div>
-      <ChatPanel
-        id={id}
-        input={input}
-        setInput={setInput}
-        isAtBottom={isAtBottom}
-        scrollToBottom={scrollToBottom}
-      />
-    </div>
-  )
+    const { messagesRef, scrollRef, visibilityRef, isAtBottom, scrollToBottom } =
+        useScrollAnchor()
+
+    return (
+        <div
+            className="bg-black group w-full overflow-auto pl-0 peer-[[data-state=open]]:lg:pl-[250px] peer-[[data-state=open]]:xl:pl-[300px]"
+            ref={scrollRef}
+        >
+            <div
+                className={cn('pb-[200px] pt-4 md:pt-10', className)}
+                ref={messagesRef}
+            >
+                {messages.length ? (
+                    <ChatList messages={messages} isShared={false} session={session} />
+                ) : (
+                    <EmptyScreen />
+                )}
+                <div className="w-full h-px" ref={visibilityRef} />
+            </div>
+            <ChatPanel
+                id={id}
+                input={input}
+                setInput={setInput}
+                isAtBottom={isAtBottom}
+                scrollToBottom={scrollToBottom}
+            />
+        </div>
+    )
 }
