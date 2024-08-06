a7891ce07f812c7a9fac1263923765abbf53a39c
Christopher David <chris@arcadelabs.co>
2024-08-06 10:09:40 -0500

    clean up chat panel

--- components/chat-panel.tsx
+++ components/chat-panel.tsx
@@ -35,28 +35,6 @@ export function ChatPanel({
     const [shareDialogOpen, setShareDialogOpen] = React.useState(false)
 
     const exampleMessages: any[] = []
-    const exampleMessagesOld = [
-        {
-            heading: 'How do I',
-            subheading: 'connect a GitHub repo?',
-            message: `How do I connect a GitHub repo?`
-        },
-        {
-            heading: 'What is the price of',
-            subheading: '$DOGE right now?',
-            message: 'What is the price of $DOGE right now?'
-        },
-        {
-            heading: 'I would like to buy',
-            subheading: '42 $DOGE',
-            message: `I would like to buy 42 $DOGE`
-        },
-        {
-            heading: 'What are some',
-            subheading: `recent events about $DOGE?`,
-            message: `What are some recent events about $DOGE?`
-        }
-    ]
 
     return (
         <div className="fixed inset-x-0 bottom-0 w-full bg-gradient-to-b from-muted/30 from-0% to-muted/30 to-50% duration-300 ease-in-out animate-in dark:from-background/10 dark:from-10% dark:to-background/80 peer-[[data-state=open]]:group-[]:lg:pl-[250px] peer-[[data-state=open]]:group-[]:xl:pl-[300px]">
