0787aff233e99b6f967d67022eb646460fe187e7
Christopher David <chris@arcadelabs.co>
2024-08-05 23:13:47 -0500

    get github token

--- lib/github/isGitHubUser.ts
+++ lib/github/isGitHubUser.ts
@@ -1,5 +1,17 @@
-import { User } from "@clerk/nextjs/server";
+import { clerkClient, User } from "@clerk/nextjs/server";
 
 export function isGitHubUser(user: User) {
     return user.externalAccounts.some(account => account.provider === 'oauth_github');
 }
+
+export async function getGitHubToken(user: User) {
+    const tokenResponse = await clerkClient().users.getUserOauthAccessToken(user.id, 'oauth_github');
+    if (tokenResponse.data.length > 0) {
+        return tokenResponse.data[0].token;
+    } else {
+        // throw error
+    }
+}
+
+
+
