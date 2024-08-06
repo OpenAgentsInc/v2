666fdcb906ae0c578555aac3f8f328a716639a12
Christopher David <chris@arcadelabs.co>
2024-08-06 18:01:35 -0500

    textsize down 1

--- components/chat-message.tsx
+++ components/chat-message.tsx
@@ -1,10 +1,8 @@
 // Inspired by Chatbot-UI and modified to fit the needs of this project
 // @see https://github.com/mckaywrigley/chatbot-ui/blob/main/components/Chat/ChatMessage.tsx
-
 import { Message } from 'ai'
 import remarkGfm from 'remark-gfm'
 import remarkMath from 'remark-math'
-
 import { cn } from '@/lib/utils'
 import { CodeBlock } from '@/components/ui/codeblock'
 import { MemoizedReactMarkdown } from '@/components/markdown'
@@ -23,7 +21,7 @@ export function ChatMessage({ message, ...props }: ChatMessageProps) {
         >
             <div
                 className={cn(
-                    'flex size-8 shrink-0 select-none items-center justify-center rounded-md border shadow',
+                    'flex size-7 shrink-0 select-none items-center justify-center rounded-md border shadow',
                     message.role === 'user'
                         ? 'bg-background'
                         : 'bg-black text-white'
@@ -31,9 +29,9 @@ export function ChatMessage({ message, ...props }: ChatMessageProps) {
             >
                 {message.role === 'user' ? <IconUser /> : <IconOpenAgents />}
             </div>
-            <div className="flex-1 px-1 ml-4 space-y-2 overflow-hidden">
+            <div className="flex-1 px-1 ml-3 space-y-1 overflow-hidden">
                 <MemoizedReactMarkdown
-                    className="prose break-words dark:prose-invert prose-p:leading-relaxed prose-pre:p-0"
+                    className="prose break-words dark:prose-invert prose-p:leading-relaxed prose-pre:p-0 text-sm"
                     remarkPlugins={[remarkGfm, remarkMath]}
                     components={{
                         p({ children }) {
@@ -46,12 +44,9 @@ export function ChatMessage({ message, ...props }: ChatMessageProps) {
                                         <span className="mt-1 cursor-default animate-pulse">▍</span>
                                     )
                                 }
-
                                 children[0] = (children[0] as string).replace('`▍`', '▍')
                             }
-
                             const match = /language-(\w+)/.exec(className || '')
-
                             if (inline) {
                                 return (
                                     <code className={className} {...props}>
@@ -59,7 +54,6 @@ export function ChatMessage({ message, ...props }: ChatMessageProps) {
                                     </code>
                                 )
                             }
-
                             return (
                                 <CodeBlock
                                     key={Math.random()}
