a7310c9fc6254b26c93a4ca6448424901a4c41fb
Christopher David <chris@arcadelabs.co>
2024-08-07 14:56:16 -0500

    button topright

--- components/dashboard/HomeDashboard.tsx
+++ components/dashboard/HomeDashboard.tsx
@@ -2,8 +2,10 @@ import { UserButton } from '@clerk/nextjs'
 
 export const HomeDashboard = () => {
     return (
-        <main className="h-screen flex items-center justify-center">
-            <UserButton />
+        <main className="h-screen relative">
+            <div className="absolute top-4 right-4">
+                <UserButton />
+            </div>
         </main>
     )
 }
