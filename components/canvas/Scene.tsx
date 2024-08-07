e90e7a186bb28f6ff2a2c655fa701fcc748df7b5
Christopher David <chris@arcadelabs.co>
2024-08-07 12:09:08 -0500

    before go to ddmrmcama version

--- components/canvas/Scene.tsx
+++ components/canvas/Scene.tsx
@@ -1,19 +1,26 @@
 'use client'
 
 import { Canvas } from '@react-three/fiber'
-import { Preload } from '@react-three/drei'
+import { Preload, Plane } from '@react-three/drei'
+import { View } from './View'
 import { r3f } from '@/helpers/global'
 import * as THREE from 'three'
 
 export default function Scene({ ...props }) {
-  // Everything defined in here will persist between route changes, only children are swapped
-  return (
-    <Canvas {...props}
-      onCreated={(state) => (state.gl.toneMapping = THREE.AgXToneMapping)}
-    >
-      {/* @ts-ignore */}
-      <r3f.Out />
-      <Preload all />
-    </Canvas>
-  )
+    // Everything defined in here will persist between route changes, only children are swapped
+    return (
+        <>
+            <View className='pointer-events-none absolute left-0 top-0 z-[-1] h-[600px] w-full bg-green-300'>
+                <Plane args={[2, 2]} position={[-2, 0, 0]} {...props} />
+            </View>
+            <Canvas {...props}
+                onCreated={(state) => (state.gl.toneMapping = THREE.AgXToneMapping)}
+            >
+                <View.Port />
+                {/* @ts-ignore */}
+                <r3f.Out />
+                <Preload all />
+            </Canvas>
+        </>
+    )
 }
