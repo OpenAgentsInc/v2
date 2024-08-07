ac6a1d668935222e0048ea7a53b8928cd8e4d803
Christopher David <chris@arcadelabs.co>
2024-08-07 13:20:53 -0500

    clerk auth nice

---
+++ app/[[...rest]]/page.tsx
@@ -0,0 +1,40 @@
+import localFont from 'next/font/local'
+import { SignIn, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import cn from 'clsx'
+
+const apfel = localFont({
+    src: '../fonts/ApfelGrotezk-Regular.woff2',
+})
+
+export default function Page() {
+    return (
+        <>
+            <SignedOut>
+                <main className={cn(apfel.className, 'pointer-events-none select-none text-white h-screen w-screen bg-black flex items-center justify-center')}>
+                    <div className='w-full max-w-md p-6'>
+                        <div className='text-center mb-6'>
+                            <h2 className={cn(apfel.className, 'tracking-wide text-xl font-bold')}>
+                                Create an account or log in
+                            </h2>
+                        </div>
+                        <div className="font-mono pointer-events-auto">
+                            <SignIn
+                                appearance={{
+                                    elements: {
+                                        formButtonPrimary: 'bg-slate-500 hover:bg-slate-400 text-sm normal-case',
+                                    },
+                                }}
+                            />
+                        </div>
+                    </div>
+                </main>
+            </SignedOut>
+            <SignedIn>
+                <main className="h-screen flex items-center justify-center">
+                    <UserButton />
+                </main>
+            </SignedIn>
+        </>
+    )
+}
+
