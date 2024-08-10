814134a70ed02d2f50909a17e619cdfbcecbb362
Christopher David <chris@arcadelabs.co>
2024-08-10 00:40:51 -0500

    Remove 0 lines in chat-panel.tsx

--- components/chat-panel.tsx
+++ components/chat-panel.tsx
@@ -77,7 +77,7 @@ export function ChatPanel({
     <div className={`flex flex-col h-full ${className}`}>
       <div className="flex-grow overflow-hidden flex flex-col">
         <div className="flex-grow overflow-auto">
-          <ChatList messages={messages} />
+          <ChatList messages={messages} isShared={false} />
         </div>
         {messages.length > 1 && (
           <div className="flex items-center justify-end p-4">
