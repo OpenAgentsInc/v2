842aef92ac6e1b9f173c0b98c4d340a18ee324c3
Christopher David <chris@arcadelabs.co>
2024-08-11 16:44:30 -0500

    stripe config

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,10 +1,12 @@
 import "server-only";
+
 import Stripe from "stripe";
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: "2023-10-16",
-  appInfo: {
-    name: "nextjs-with-stripe-typescript-demo",
-    url: "https://nextjs-with-stripe-typescript-demo.vercel.app",
-  },
+    // https://github.com/stripe/stripe-node#configuration
+    apiVersion: "2023-10-16",
+    appInfo: {
+        name: "openagents",
+        url: "https://openagents.com"
+    },
 });
