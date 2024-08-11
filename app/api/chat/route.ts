1f4c758718300065f52c600be4b2d29648fa7156
Christopher David <chris@arcadelabs.co>
2024-08-11 01:00:03 -0500

    dupe user msg mayb

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,9 +1,7 @@
 import { convertToCoreMessages, streamText } from 'ai';
 import { getSystemPrompt } from '@/lib/systemPrompt';
 import { getTools, getToolContext } from '@/tools';
-import { onFinish } from './onFinish';
 import { auth } from '@clerk/nextjs/server';
-import { Message } from '@/types';
 
 export const dynamic = 'force-dynamic';
 export const maxDuration = 60;
@@ -24,27 +22,11 @@ export async function POST(req: Request) {
     }
 
     const messages = convertToCoreMessages(body.messages);
-    const userMessage = messages[messages.length - 1] as Message;
     const result = await streamText({
         messages,
         model: toolContext.model,
         system: getSystemPrompt(toolContext),
         tools,
-        onFinish: (finishResult) => {
-            console.log('Finish result:', finishResult);
-            const assistantMessage: Message = {
-                id: Date.now().toString(),
-                role: 'assistant',
-                content: finishResult.text,
-            };
-            onFinish({
-                ...finishResult,
-                threadId,
-                clerkUserId: userId,
-                userMessage,
-                assistantMessage,
-            });
-        },
     });
     return result.toAIStreamResponse();
 }
