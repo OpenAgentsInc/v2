176cd66ac3b9d9d6d0f26d780648edbe14147156
Christopher David <chris@arcadelabs.co>
2024-08-11 10:55:34 -0500

    Add 11 lines in saveMessage.ts

--- db/actions/saveMessage.ts
+++ db/actions/saveMessage.ts
@@ -2,17 +2,29 @@
 import { sql } from '@vercel/postgres'
 import { Message, OnFinishOptions } from '@/types'
 
-export async function saveMessage(threadId: number, clerkUserId: string, message: Message, options: OnFinishOptions) {
+export async function saveMessage(threadId: number, clerkUserId: string, message: Message, options?: OnFinishOptions) {
     try {
         console.log("IN DB saveMessage WITH MESSAGE AND OPtions:", message, options);
         const contentString = typeof message.content === 'string' ? message.content : JSON.stringify(message.content);
         const toolInvocationsString = message.toolInvocations ? JSON.stringify(message.toolInvocations) : null;
 
-        const { rows } = await sql`
-        INSERT INTO messages (thread_id, clerk_user_id, role, content, tool_invocations, finish_reason, total_tokens, prompt_tokens, completion_tokens)
-        VALUES (${threadId}, ${clerkUserId}, ${message.role}, ${contentString}, ${toolInvocationsString}::jsonb, ${options.finishReason}, ${options.usage.totalTokens}, ${options.usage.promptTokens}, ${options.usage.completionTokens})
+        let query = sql`
+        INSERT INTO messages (thread_id, clerk_user_id, role, content, tool_invocations
+        `;
+
+        let values = [threadId, clerkUserId, message.role, contentString, sql`${toolInvocationsString}::jsonb`];
+
+        if (options) {
+            query = sql`${query}, finish_reason, total_tokens, prompt_tokens, completion_tokens`;
+            values.push(options.finishReason, options.usage.totalTokens, options.usage.promptTokens, options.usage.completionTokens);
+        }
+
+        query = sql`${query})
+        VALUES (${sql.join(values, sql`, `)})
         RETURNING id, created_at as "createdAt", tool_invocations as "toolInvocations"
         `;
+
+        const { rows } = await query;
 
         const savedMessage = {
             ...message,
