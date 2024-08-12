5948b8e3610db85dbcc4ec4677aab4b41f51c3cc
Christopher David <chris@arcadelabs.co>
2024-08-12 01:01:02 -0500

    Add 15 lines in markdown.tsx

--- components/chat/markdown.tsx
+++ components/chat/markdown.tsx
@@ -1,8 +1,24 @@
-import { FC, memo } from 'react'
+import { FC, memo, useState, useEffect } from 'react'
 import ReactMarkdown, { Options } from 'react-markdown'
+import { useDebounce } from 'use-debounce'
 
-export const MemoizedReactMarkdown: FC<Options> = memo(
-  ReactMarkdown,
+interface MemoizedReactMarkdownProps extends Options {
+  children: string
+  delay?: number
+  maxWait?: number
+}
+
+export const MemoizedReactMarkdown: FC<MemoizedReactMarkdownProps> = memo(
+  ({ children, delay = 250, maxWait = 1000, ...props }) => {
+    const [content, setContent] = useState(children)
+    const [debouncedContent] = useDebounce(content, delay, { maxWait })
+
+    useEffect(() => {
+      setContent(children)
+    }, [children])
+
+    return <ReactMarkdown {...props}>{debouncedContent}</ReactMarkdown>
+  },
   (prevProps, nextProps) =>
     prevProps.children === nextProps.children &&
     prevProps.className === nextProps.className
