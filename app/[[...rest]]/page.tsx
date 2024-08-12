d8a7e688a466903f1003dfdc07ff3ba6d74ee18f
Christopher David <chris@arcadelabs.co>
2024-08-11 21:20:15 -0500

    move page out of catchall, add catchall redirect

--- app/[[...rest]]/page.tsx
+++ app/[[...rest]]/page.tsx
@@ -1,16 +1,5 @@
-import { SignedIn, SignedOut } from '@clerk/nextjs'
-import { HomeAuthed, HomeUnauthed } from '@/components/home'
+import { redirect } from 'next/navigation';
 
-export default function Page() {
-    return (
-        <>
-            <SignedOut>
-                <HomeUnauthed />
-            </SignedOut>
-            <SignedIn>
-                <HomeAuthed />
-            </SignedIn>
-        </>
-    )
+export default function CatchAllRoute() {
+    redirect('/');
 }
-
