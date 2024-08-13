b62ec7d55a6158075ff7d8225836e8b663ed9f8e
Christopher David <chris@arcadelabs.co>
2024-08-13 10:19:21 -0500

    Sent first email

---
+++ app/api/send/route.ts
@@ -0,0 +1,25 @@
+import { EmailTemplate } from '@/components/email/email-template';
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function GET() {
+    try {
+        const { data, error } = await resend.emails.send({
+            from: 'Chris at OpenAgents <chris@openagents.com>',
+            to: ['kikidesignsfun@gmail.com'],
+            cc: ['chris+email@openagents.com'],
+            subject: 'I love you',
+            react: EmailTemplate({ firstName: 'Kiki' }),
+        });
+
+        if (error) {
+            return Response.json({ error }, { status: 500 });
+        }
+
+        return Response.json(data);
+    } catch (error) {
+        return Response.json({ error }, { status: 500 });
+    }
+}
+
