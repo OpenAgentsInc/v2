f2f45dc514eee92bb26b5eae204020c8546f4c05
Christopher David <chris@openagents.com>
2024-08-14 12:42:26 -0500

    documentcard

--- components/knowledge/Knowledge.tsx
+++ components/knowledge/Knowledge.tsx
@@ -1,15 +1,20 @@
 import { api } from "@/convex/_generated/api"
 import { useQuery } from "convex/react"
 import { CreateDocumentButton } from "./CreateDocumentButton"
+import { DocumentCard } from "./DocumentCard"
 
 export const Knowledge = () => {
   const documents = useQuery(api.documents.getDocuments)
   return (
-    <div className="h-full flex flex-col items-center justify-center">
-      <CreateDocumentButton />
-      {documents?.map((doc) => (
-        <div key={doc._id}>{doc.title}</div>
-      ))}
+    <div className="p-6 space-y-8">
+      <div className="flex justify-between items-center">
+        <h1 className="text-xl font-bold">My Documents</h1>
+        <CreateDocumentButton />
+      </div>
+
+      <div className="grid grid-cols-4 gap-8">
+        {documents?.map((doc) => <DocumentCard document={doc} key={doc._id} />)}
+      </div>
     </div>
   )
 }
