364b2342499c1bcb747e477806414223c4797d91
Christopher David <chris@openagents.com>
2024-08-14 15:11:52 -0500

    Add 14 lines in schema.ts

--- convex/schema.ts
+++ convex/schema.ts
@@ -6,5 +6,20 @@ export default defineSchema({
     title: v.string(),
     tokenIdentifier: v.string(),
     fileId: v.id("_storage"),
-  }).index('by_tokenIdentifier', ['tokenIdentifier'])
+  }).index('by_tokenIdentifier', ['tokenIdentifier']),
+
+  sbv_datum: defineTable({
+    instance_id: v.string(),
+    patch: v.string(),
+    repo: v.string(),
+    base_commit: v.string(),
+    hints_text: v.string(),
+    created_at: v.string(),
+    test_patch: v.string(),
+    problem_statement: v.string(),
+    version: v.string(),
+    environment_setup_commit: v.string(),
+    FAIL_TO_PASS: v.string(),
+    PASS_TO_PASS: v.string(),
+  }).index('by_instance_id', ['instance_id'])
 });
