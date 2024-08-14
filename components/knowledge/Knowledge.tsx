37bbf49b059be7f1a178113185b21b7df1a975a6
Christopher David <chris@openagents.com>
2024-08-14 12:10:58 -0500

    hello world mutation

--- components/knowledge/Knowledge.tsx
+++ components/knowledge/Knowledge.tsx
@@ -1,9 +1,15 @@
+import { api } from "@/convex/_generated/api"
 import { Button } from "../ui/button"
+import { useMutation } from "convex/react"
 
 export const Knowledge = () => {
+  const createDocument = useMutation(api.documents.createDocument)
+  const handleClick = async () => {
+    await createDocument({ title: 'Hello world' })
+  }
   return (
     <div className="h-full flex flex-col items-center justify-center">
-      <Button onClick={() => console.log('clicked')}>Click me</Button>
+      <Button onClick={handleClick}>Click me</Button>
     </div>
   )
 }
