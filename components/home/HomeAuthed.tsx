4ca9565110c2d92cabdc0716902db7ae09dce653
Christopher David <chris@openagents.com>
2024-08-14 11:22:05 -0500

    HomeAuthed useclient

--- components/home/HomeAuthed.tsx
+++ components/home/HomeAuthed.tsx
@@ -1,8 +1,20 @@
+"use client"
+
 import { Hud } from '@/components/hud/hud'
+import { Pane } from '@/components/hud/pane'
+import { ChatHistory } from '@/components/sidebar/chat-history'
+import { useAuth } from "@clerk/nextjs"
 
 export const HomeAuthed = () => {
+  const { userId } = useAuth()
+  if (!userId) {
+    return <></>
+  }
   return (
     <main className="h-screen flex items-center justify-center relative">
+      <Pane title="Chat History" id={0} x={90} y={190} height={450} width={260} dismissable={false}>
+        <ChatHistory userId={userId} />
+      </Pane>
       <Hud />
     </main>
   )
--- components/hud/hud.tsx
+++ components/hud/hud.tsx
@@ -6,24 +6,24 @@ import { Pane } from '@/components/hud/pane'
 import { UserStatus } from './UserStatus'
 
 export const Hud = () => {
-    const { panes } = useHudStore()
+  const { panes } = useHudStore()
 
-    return (
-        <div>
-            <UserStatus />
-            {panes.map((pane) => (
-                <Pane
-                    key={pane.id}
-                    title={pane.title}
-                    id={pane.id}
-                    x={pane.x}
-                    y={pane.y}
-                    height={pane.height}
-                    width={pane.width}
-                >
-                    {pane.type === 'chat' && <Chat threadId={pane.id} />}
-                </Pane>
-            ))}
-        </div>
-    )
+  return (
+    <div>
+      <UserStatus />
+      {panes.map((pane) => (
+        <Pane
+          key={pane.id}
+          title={pane.title}
+          id={pane.id}
+          x={pane.x}
+          y={pane.y}
+          height={pane.height}
+          width={pane.width}
+        >
+          {pane.type === 'chat' && <Chat threadId={pane.id} />}
+        </Pane>
+      ))}
+    </div>
+  )
 }
