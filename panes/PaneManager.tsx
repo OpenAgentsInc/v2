e32d729c4ce92e061eefd71183e6b185e489e7b6
Christopher David <chris@openagents.com>
2024-08-15 16:26:34 -0500

    Create file panes/PaneManager.tsx

---
+++ panes/PaneManager.tsx
@@ -0,0 +1,48 @@
+"use client"
+
+import { usePaneStore } from '@/store/pane'
+import { Chat } from '@/components/chat'
+import { Pane as PaneComponent } from '@/panes/Pane'
+import { UserStatus } from '@/panes/user/UserStatus'
+import { Id } from '@/convex/_generated/dataModel'
+import { Pane } from '@/types/pane'
+
+export const PaneManager = () => {
+  const { panes } = usePaneStore()
+
+  const stripChatPrefix = (id: string): string => {
+    // Remove 'chat-' prefix up to two times
+    let strippedId = id.replace(/^chat-/, '').replace(/^chat-/, '')
+    return strippedId
+  }
+
+  return (
+    <>
+      <UserStatus />
+      {panes.map((pane: Pane) => (
+        <PaneComponent
+          key={pane.id}
+          title={pane.title}
+          id={stripChatPrefix(pane.id)}
+          x={pane.x}
+          y={pane.y}
+          height={pane.height}
+          width={pane.width}
+          type={pane.type}
+          content={pane.content}
+          isActive={pane.isActive}
+        >
+          {pane.type === 'chat' && <Chat threadId={stripChatPrefix(pane.id) as Id<"threads">} />}
+          {pane.type === 'diff' && pane.content && (
+            <div>
+              <h3>Old Content:</h3>
+              <pre>{pane.content.oldContent}</pre>
+              <h3>New Content:</h3>
+              <pre>{pane.content.newContent}</pre>
+            </div>
+          )}
+        </PaneComponent>
+      ))}
+    </>
+  )
+}
