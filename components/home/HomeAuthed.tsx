b31877c58936d90983ae9426ae60557773c283f4
Christopher David <chris@openagents.com>
2024-08-15 16:30:13 -0500

    Add 13 lines in HomeAuthed.tsx

--- components/home/HomeAuthed.tsx
+++ components/home/HomeAuthed.tsx
@@ -1,19 +1,33 @@
 import React from 'react'
-import { Pane } from '@/components/hud/pane'
+import { Pane } from '@/panes/Pane'
 import { ChatHistory } from '@/components/sidebar/chat-history'
 import { useUser } from '@clerk/nextjs'
-import { Hud } from '../hud/hud'
+import { PaneManager } from '@/panes/PaneManager'
+import { usePaneStore } from '@/store/pane'
 
 export function HomeAuthed() {
   const { user } = useUser()
+  const addPane = usePaneStore(state => state.addPane)
+
   if (!user) return null
+
+  React.useEffect(() => {
+    addPane({
+      id: 'chat-history',
+      title: 'Chat History',
+      type: 'default',
+      x: 20,
+      y: 190,
+      height: 350,
+      width: 260,
+      dismissable: false,
+      content: <ChatHistory clerkUserId={user.id} />
+    })
+  }, [user.id, addPane])
 
   return (
     <div className="relative w-full h-full">
-      <Pane title="Chat History" id="chat-history" type="default" x={20} y={190} height={350} width={260} dismissable={false}>
-        <ChatHistory clerkUserId={user.id} />
-      </Pane>
-      <Hud />
+      <PaneManager />
     </div>
   )
 }
