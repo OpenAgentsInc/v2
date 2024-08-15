669e26861d1b96bf965e041c38e14f3d938fe209
Christopher David <chris@openagents.com>
2024-08-15 08:19:51 -0500

    Add 3 lines in getUserBalance.ts

--- convex/getUserBalance.ts
+++ convex/getUserBalance.ts
@@ -1,11 +1,14 @@
 import { query } from './_generated/server';
 import { v } from 'convex/values';
 
-export default query(async ({ db }, clerk_user_id: string) => {
-  const user = await db
-    .query('users')
-    .withIndex('by_clerk_user_id', (q) => q.eq('clerk_user_id', clerk_user_id))
-    .first();
+export default query({
+  args: { clerk_user_id: v.string() },
+  handler: async ({ db }, { clerk_user_id }) => {
+    const user = await db
+      .query('users')
+      .withIndex('by_clerk_user_id', (q) => q.eq('clerk_user_id', clerk_user_id))
+      .first();
 
-  return user?.credits ?? 0;
+    return user?.credits ?? 0;
+  },
 });
