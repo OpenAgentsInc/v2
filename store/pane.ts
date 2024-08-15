4fe5c50ee2425a3d150acf86d662d211da5e67c8
Christopher David <chris@openagents.com>
2024-08-15 17:51:37 -0500

    Remove 1 lines in pane.ts

--- store/pane.ts
+++ store/pane.ts
@@ -1,9 +1,8 @@
 import { create } from "zustand"
 import { persist } from "zustand/middleware"
-import { Pane, PaneInput } from '@/types/pane'
 import { calculatePanePosition } from './paneUtils'
 import * as actions from './hudActions'
-import { PaneStore } from './types'
+import { PaneStore, PaneInput } from './types'
 
 export const usePaneStore = create<PaneStore>()(
   persist(
@@ -12,7 +11,7 @@ export const usePaneStore = create<PaneStore>()(
       isChatOpen: true,
       activeTerminalId: null,
       lastPanePosition: null,
-      addPane: (newPane: PaneInput, shouldTile = false) => actions.addPane(set, newPane, shouldTile),
+      addPane: (pane, shouldTile = false) => actions.addPane(set, pane, shouldTile),
       removePane: (id) => actions.removePane(set, id),
       updatePanePosition: (id, x, y) => actions.updatePanePosition(set, id, x, y),
       updatePaneSize: (id, width, height) => actions.updatePaneSize(set, id, width, height),
@@ -22,7 +21,7 @@ export const usePaneStore = create<PaneStore>()(
       setInputFocused: (isFocused: boolean) => set({ isInputFocused: isFocused }),
       isRepoInputOpen: false,
       setRepoInputOpen: (isOpen: boolean) => set({ isRepoInputOpen: isOpen }),
-      openChatPane: (newPane: PaneInput) => actions.openChatPane(set, newPane),
+      openChatPane: (pane) => actions.openChatPane(set, pane),
       bringPaneToFront: (id) => actions.bringPaneToFront(set, id),
       setActivePane: (id) => actions.setActivePane(set, id),
     }),
