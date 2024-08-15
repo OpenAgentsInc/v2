7852f33303e137b2c45b18004c67acce969add20
Christopher David <chris@openagents.com>
2024-08-14 20:00:41 -0500

    Add 2 lines in swebench.ts

--- convex/swebench.ts
+++ convex/swebench.ts
@@ -2,7 +2,10 @@ import { query } from "./_generated/server";
 
 export const getAllSWEData = query({
   handler: async (ctx) => {
-    const allData = await ctx.db.query("sbv_datum").collect();
+    const allData = await ctx.db
+      .query("sbv_datum")
+      .filter((q) => q.eq(q.field("repo"), "psf/requests"))
+      .collect();
     return allData;
   },
 });
