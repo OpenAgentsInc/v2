8f8a1e629660bbbaa9893dd84fd6b612703483ec
Christopher David <chris@openagents.com>
2024-08-15 17:54:38 -0500

    Remove 25 lines in types.ts

--- store/types.ts
+++ store/types.ts
@@ -1,29 +1,5 @@
 import { Id } from '@/convex/_generated/dataModel'
-
-export type Pane = {
-  id: Id<"threads"> | number
-  title: string
-  x: number
-  y: number
-  width: number
-  height: number
-  type: 'default' | 'chat' | 'diff'
-  content?: {
-    oldContent?: string
-    newContent?: string
-  }
-  isActive?: boolean
-}
-
-export type PaneInput = Omit<Pane, 'x' | 'y' | 'width' | 'height' | 'id'> & {
-  id?: Id<"threads"> | number;
-  paneProps?: {
-    x: number
-    y: number
-    width: number
-    height: number
-  }
-}
+import { Pane, PaneInput } from '@/types/pane'
 
 export type PaneStore = {
   panes: Pane[]
