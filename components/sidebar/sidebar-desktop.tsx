42b944314b1270ee7159c0657d7e1eb7c2924b96
Christopher David <chris@openagents.com>
2024-08-15 00:34:50 -0500

    Remove 0 lines in sidebar-desktop.tsx

--- components/sidebar/sidebar-desktop.tsx
+++ components/sidebar/sidebar-desktop.tsx
@@ -3,9 +3,10 @@
 import { Sidebar } from './sidebar'
 import { ChatHistory } from './chat-history'
 import { auth } from '@clerk/nextjs/server'
+import { Id } from '@/convex/_generated/dataModel'
 
 export async function SidebarDesktop() {
-    const userId = auth().userId
+    const { userId } = auth()
 
     if (!userId) {
         return null
@@ -13,7 +14,7 @@ export async function SidebarDesktop() {
 
     return (
         <Sidebar className="peer absolute inset-y-0 z-30 hidden -translate-x-full border-r bg-muted duration-300 ease-in-out data-[state=open]:translate-x-0 lg:flex lg:w-[250px] xl:w-[300px]">
-            <ChatHistory userId={userId} />
+            <ChatHistory userId={userId as Id<"users">} clerkUserId={userId} />
         </Sidebar>
     )
 }
