510f7887bf2980b509b568f18569ad4a96f97f1d
Christopher David <chris@openagents.com>
2024-08-15 07:41:58 -0500

    Remove 1 lines in chat-list.tsx

--- components/chat/chat-list.tsx
+++ components/chat/chat-list.tsx
@@ -14,9 +14,9 @@ export function ChatList({ messages }: ChatListProps) {
     return (
         <div className="overflow-y-scroll p-6 px-12 -ml-4 w-full">
             {messages.map((message, index) => (
-                <div key={message.id}>
-                    <ChatMessage message={message} />
-                    {index < messages.length - 1 && <Separator className="my-4" />}
+                <div key={`${message.id}-${index}`}>
+                    <ChatMessage key={`message-${message.id}-${index}`} message={message} />
+                    {index < messages.length - 1 && <Separator key={`separator-${index}`} className="my-4" />}
                 </div>
             ))}
         </div>
