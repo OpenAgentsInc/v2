aa54adbc2ac448c1080aeb889120aec99de682d7
Christopher David <chris@openagents.com>
2024-08-14 23:20:57 -0500

    Remove 0 lines in message.ts

--- types/message.ts
+++ types/message.ts
@@ -11,7 +11,7 @@ export interface Message extends Partial<SDKMessage> {
     /**
      * The timestamp of the message.
      */
-    createdAt?: Date | string;
+    createdAt?: Date;
 
     /**
      * Text content of the message.
