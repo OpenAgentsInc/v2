c31eb12715e9c6b1517887de7a204a0d3648b890
Christopher David <chris@openagents.com>
2024-08-15 00:33:49 -0500

    Remove 0 lines in HomeAuthed.tsx

--- components/home/HomeAuthed.tsx
+++ components/home/HomeAuthed.tsx
@@ -5,6 +5,7 @@ import { Pane } from '@/components/hud/pane'
 import { ChatHistory } from '@/components/sidebar/chat-history'
 import { useAuth } from "@clerk/nextjs"
 import { Knowledge } from '../knowledge/Knowledge'
+import { Id } from '@/convex/_generated/dataModel'
 
 export const HomeAuthed = () => {
   const { userId } = useAuth()
@@ -14,7 +15,7 @@ export const HomeAuthed = () => {
   return (
     <main className="h-screen flex items-center justify-center relative">
       <Pane title="Chat History" id={0} x={20} y={190} height={350} width={260} dismissable={false}>
-        <ChatHistory userId={userId} />
+        <ChatHistory userId={userId as Id<"users">} clerkUserId={userId} />
       </Pane>
       {/* <Pane title="Knowledge" id={-1} x={350} y={90} height={650} width={860} dismissable={false}>
         <Knowledge />
