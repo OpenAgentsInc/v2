fff2e1ccec6cc10431deded996c154255cb8f57b
Christopher David <chris@openagents.com>
2024-08-15 22:50:21 -0500

    Remove 0 lines in hudActions.ts

--- store/hudActions.ts
+++ store/hudActions.ts
@@ -37,7 +37,7 @@ export function addPane(set: (fn: (state: PaneStore) => Partial<PaneStore>) => v
     }
 
     const adjustedPosition = adjustPanePosition(panePosition)
-    const newPaneWithPosition = createNewPaneWithPosition(newPane, newPane.id, adjustedPosition)
+    const newPaneWithPosition = createNewPaneWithPosition(newPane, newPane.id || '', adjustedPosition)
 
     return {
       panes: [...updatedPanes.map(pane => ({ ...pane, isActive: false })), newPaneWithPosition],
