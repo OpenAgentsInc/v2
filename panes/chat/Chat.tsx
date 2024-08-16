bb0d8f0aefa5e889a8895fc090c4c7ed90f7838e
Christopher David <chris@openagents.com>
2024-08-16 15:15:34 -0500

    hover bg black on stuff

--- panes/chat/Chat.tsx
+++ panes/chat/Chat.tsx
@@ -8,28 +8,27 @@ import { Id } from '@/convex/_generated/dataModel'
 import { Thread } from '@/panes/chat/chatUtils'
 
 export interface ChatProps extends React.ComponentProps<'div'> {
-    threadId: Id<"threads">
+  threadId: Id<"threads">
 }
 
 export const Chat = ({ threadId, className }: ChatProps) => {
-    const {
-        messages,
-        sendMessage,
-        isLoading
-    } = useChat(threadId)
+  const {
+    messages,
+    sendMessage,
+    isLoading
+  } = useChat(threadId)
+  const chatContainerRef = useChatScroll(messages);
 
-    const chatContainerRef = useChatScroll(messages);
-
-    return (
-        <div className={`flex flex-col h-full ${className}`}>
-            <div className="flex-1 overflow-auto" ref={chatContainerRef}>
-                <ChatList messages={messages as Message[]} />
-            </div>
-            <div className="flex-shrink-0 w-full">
-                <div className="sticky bottom-0 w-full">
-                    <InputBar onSubmit={sendMessage} isLoading={isLoading} />
-                </div>
-            </div>
+  return (
+    <div className={`bg-transparent hover:bg-black hover:bg-opacity-100 transition-all duration-300 ease-in-out flex flex-col h-full ${className}`}>
+      <div className="flex-1 overflow-auto" ref={chatContainerRef}>
+        <ChatList messages={messages as Message[]} />
+      </div>
+      <div className="flex-shrink-0 w-full">
+        <div className="sticky bottom-0 w-full">
+          <InputBar onSubmit={sendMessage} isLoading={isLoading} />
         </div>
-    )
+      </div>
+    </div>
+  )
 }
