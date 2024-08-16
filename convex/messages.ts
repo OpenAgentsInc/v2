726647f2f39765817466a9bebfc5fae16b16776c
Christopher David <chris@openagents.com>
2024-08-16 14:32:22 -0500

    axe convex messages

--- convex/messages.ts
+++
@@ -1,4 +0,0 @@
-export { saveChatMessage } from "./messages/saveChatMessage";
-export { fetchThreadMessages } from "./messages/fetchThreadMessages";
-export { getLastMessage } from "./messages/getLastMessage";
-export { getChatById } from "./messages/getChatById";
