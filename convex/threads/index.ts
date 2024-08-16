9ee9c63af9230e91a89010c92f3fe57dca0df393
Christopher David <chris@openagents.com>
2024-08-16 14:15:04 -0500

    Create file convex/threads/index.ts

---
+++ convex/threads/index.ts
@@ -0,0 +1,21 @@
+import { createOrGetUser } from './createOrGetUser';
+import { createNewThread } from './createNewThread';
+import { getUserThreads } from './getUserThreads';
+import { getLastEmptyThread } from './getLastEmptyThread';
+import { deleteThread } from './deleteThread';
+import { updateThreadData } from './updateThreadData';
+import { shareThread } from './shareThread';
+import { generateTitle } from './generateTitle';
+import { getThreadMessages } from './getThreadMessages';
+
+export {
+  createOrGetUser,
+  createNewThread,
+  getUserThreads,
+  getLastEmptyThread,
+  deleteThread,
+  updateThreadData,
+  shareThread,
+  generateTitle,
+  getThreadMessages,
+};
