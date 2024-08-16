df406567896bf1fee4767d17b9c1454d1984fb96
Christopher David <chris@openagents.com>
2024-08-16 17:35:37 -0500

    Remove 0 lines in getChatById.ts

--- convex/messages/getChatById.ts
+++ convex/messages/getChatById.ts
@@ -3,10 +3,10 @@ import { Id } from '@/convex/_generated/dataModel';
 
 export const getChatById = query({
   args: {
-    chatId: Id<'messages'>
+    chatId: 'id'
   },
   handler: async ({ db }, { chatId }) => {
-    const chat = await db.get(chatId);
+    const chat = await db.get(chatId as Id<'messages'>);
     if (!chat) {
       throw new Error(`Chat with id ${chatId} not found`);
     }
