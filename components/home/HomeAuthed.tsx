3808e320d4b331f3b51f9f63a89b83e45ffad4db
Christopher David <chris@openagents.com>
2024-08-18 08:55:38 -0500

    Add 1 lines in HomeAuthed.tsx

--- components/home/HomeAuthed.tsx
+++ components/home/HomeAuthed.tsx
@@ -1,8 +1,10 @@
 import { PaneManager } from '@/panes/PaneManager'
+import { SimpleGrid } from '@/components/ui/simple-grid'
 
 export function HomeAuthed() {
   return (
     <div className="relative w-full h-full">
+      <SimpleGrid />
       <PaneManager />
     </div>
   )
