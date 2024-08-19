ff10358cadd69e31cc1edcbab3358f7609f5e77c
Christopher David <chris@openagents.com>
2024-08-19 10:23:36 -0400

    Remove 0 lines in schema.ts

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,4 +1,4 @@
-import { defineSchema } from "convex/server";
+import { defineSchema, defineTable } from "convex/server";
 import { documents } from "./schema/documents";
 import { sbv_datum } from "./schema/sbv_datum";
 import { users } from "./schema/users";
@@ -12,9 +12,9 @@ export default defineSchema({
   users,
   threads,
   messages,
-  emails,
-  emailCampaigns,
-  emailAudiences,
+  emails: defineTable(emails),
+  emailCampaigns: defineTable(emailCampaigns),
+  emailAudiences: defineTable(emailAudiences),
 });
 
-export { documents, sbv_datum, users, threads, messages, emails, emailCampaigns, emailAudiences };
+export { documents, sbv_datum, users, threads, messages };
