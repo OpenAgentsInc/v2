51b5014fda5e46910dd6d41ec587f7492972fc52
Christopher David <chris@openagents.com>
2024-08-21 17:27:09 -0400

    Remove 0 lines in route.ts

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,11 +1,12 @@
 import { serve } from "inngest/next"
-import { helloWorld } from "@/inngest/functions"
+import { helloWorld, processMessage } from "@/inngest/functions"
 import { inngest } from "../../../inngest/client"
 
-// Create an API that serves zero functions
+// Create an API that serves our functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-    helloWorld
+    helloWorld,
+    processMessage
   ],
 });
