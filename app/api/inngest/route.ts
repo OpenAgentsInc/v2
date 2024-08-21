68875e8b862d73c0bfc249ae2826476ac635c5a8
Christopher David <chris@openagents.com>
2024-08-21 17:29:22 -0400

    nice

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,6 +1,6 @@
 import { serve } from "inngest/next"
+import { inngest } from "@/inngest/client"
 import { helloWorld, processMessage } from "@/inngest/functions"
-import { inngest } from "../../../inngest/client"
 
 // Create an API that serves our functions
 export const { GET, POST, PUT } = serve({
