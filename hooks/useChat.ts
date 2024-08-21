43291b478e6b8eb6fea5c3dbcf87ebac4bace4c2
Christopher David <chris@openagents.com>
2024-08-21 19:33:50 -0400

    support loading

--- hooks/useChat.ts
+++ hooks/useChat.ts
@@ -1,4 +1,5 @@
 import { useQuery } from "convex/react"
+import { useCallback, useState } from "react"
 import { api } from "@/convex/_generated/api"
 import { Id } from "@/convex/_generated/dataModel"
 import { sendMessage as libSendMessage } from "@/lib/chat/sendMessage"
@@ -8,12 +9,18 @@ interface UseChatProps {
 }
 
 export function useChat({ threadId }: UseChatProps) {
+  const [isLoading, setIsLoading] = useState(false)
   const messages = useQuery(api.messages.fetchThreadMessages.fetchThreadMessages, { thread_id: threadId as Id<"threads"> });
-  const sendMessage = async (text: string) => {
-    libSendMessage({ text, threadId })
-  }
+
+  const sendMessage = useCallback(async (text: string) => {
+    setIsLoading(true)
+    const res = await libSendMessage({ text, threadId })
+    console.log("libSendMessage response", res)
+    setIsLoading(false)
+  }, [threadId, setIsLoading])
+
   return {
-    isLoading: false,
+    isLoading,
     messages: messages || [],
     sendMessage
   }
