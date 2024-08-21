834ed0cb6fd4b6d89ae2a9ee782765353ed38012
Christopher David <chris@openagents.com>
2024-08-21 19:22:30 -0400

    useChat messages with the threadId, nice

--- hooks/useChat.ts
+++ hooks/useChat.ts
@@ -1,11 +1,16 @@
+import { useQuery } from "convex/react"
+import { api } from "@/convex/_generated/api"
+import { Id } from "@/convex/_generated/dataModel"
+
 interface UseChatProps {
   threadId: string
 }
 
 export function useChat({ threadId }: UseChatProps) {
+  const messages = useQuery(api.messages.fetchThreadMessages.fetchThreadMessages, { thread_id: threadId as Id<"threads"> });
   return {
     isLoading: false,
-    messages: [],
+    messages: messages || [],
     sendMessage: () => { }
   }
 }
