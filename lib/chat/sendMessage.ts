c1dcec6584817f15976e41288763d6cf42b6f6a4
Christopher David <chris@openagents.com>
2024-08-21 22:03:53 -0400

    no default modelId

--- lib/chat/sendMessage.ts
+++ lib/chat/sendMessage.ts
@@ -4,12 +4,12 @@ import { authenticateUser } from "./authenticateUser"
 import { triggerInngestEvent } from "./triggerInngestEvent"
 
 interface SendMessageProps {
-  modelId?: string
+  modelId: string
   text: string
   threadId: string
 }
 
-export async function sendMessage({ modelId = 'anthropic.claude-3-5-sonnet-20240620-v1:0', text, threadId }: SendMessageProps) {
+export async function sendMessage({ modelId, text, threadId }: SendMessageProps) {
   const user = await authenticateUser()
 
   await triggerInngestEvent({
