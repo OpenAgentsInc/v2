b7a3847d95dbf2fa70636da2b4d079b9ddcb68c2
Christopher David <chris@openagents.com>
2024-08-22 11:54:41 -0500

    hmm

--- types/on-chunk-result.ts
+++ types/on-chunk-result.ts
@@ -1,24 +1,31 @@
 export type OnChunkResult = {
   chunk:
-    | {
-        type: "text-delta";
-        textDelta: string;
-      }
-    | {
-        type: "tool-call-streaming-start";
-        toolCallId: string;
-        toolName: string;
-      }
-    | {
-        type: "tool-call-delta";
-        toolCallId: string;
-        toolName: string;
-        argsTextDelta: string;
-      }
-    | ({
-        type: "tool-call";
-        toolCallId: string;
-        toolName: string;
-        args: Record<string, any>;
-      } & { content: string });
+  | {
+    type: "tool-result";
+    toolCallId: string;
+    toolName: string;
+    args: Record<string, any>;
+    result: any;
+  }
+  | {
+    type: "text-delta";
+    textDelta: string;
+  }
+  | {
+    type: "tool-call-streaming-start";
+    toolCallId: string;
+    toolName: string;
+  }
+  | {
+    type: "tool-call-delta";
+    toolCallId: string;
+    toolName: string;
+    argsTextDelta: string;
+  }
+  | ({
+    type: "tool-call";
+    toolCallId: string;
+    toolName: string;
+    args: Record<string, any>;
+  } & { content: string });
 };
