48798e2cfe110b35d67a87f24b591212e898b6f3
Christopher David <chris@openagents.com>
2024-08-22 13:01:09 -0400

    Remove 12 lines in on-chunk-result.ts

--- types/on-chunk-result.ts
+++ types/on-chunk-result.ts
@@ -1,31 +1,20 @@
 export type OnChunkResult = {
-  chunk:
-  | {
-    type: "tool-result";
-    toolCallId: string;
-    toolName: string;
-    args: Record<string, any>;
-    result: any;
+  chunk: { 
+    type: "text-delta"; 
+    textDelta: string; 
+  } | { 
+    type: "tool-call-streaming-start"; 
+    toolCallId: string; 
+    toolName: string; 
+  } | { 
+    type: "tool-call-delta"; 
+    toolCallId: string; 
+    toolName: string; 
+    argsTextDelta: string; 
+  } | { 
+    type: "tool-call"; 
+    toolCallId: string; 
+    toolName: string; 
+    args: Record<string, any>; 
   }
-  | {
-    type: "text-delta";
-    textDelta: string;
-  }
-  | {
-    type: "tool-call-streaming-start";
-    toolCallId: string;
-    toolName: string;
-  }
-  | {
-    type: "tool-call-delta";
-    toolCallId: string;
-    toolName: string;
-    argsTextDelta: string;
-  }
-  | ({
-    type: "tool-call";
-    toolCallId: string;
-    toolName: string;
-    args: Record<string, any>;
-  } & { content: string });
 };
