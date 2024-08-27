cd529e7bcb2a7ad68b53cebd440d574ac009adb7
Christopher David <chris@openagents.com>
2024-08-27 13:07:00 -0500

    Add 1 lines in index.ts

--- tools/index.ts
+++ tools/index.ts
@@ -18,6 +18,7 @@ import { scrapeWebpageTool } from "./scrape-webpage"
 import { updatePullRequestTool } from "./update-pull-request"
 import { viewFileTool } from "./view-file"
 import { viewHierarchyTool } from "./view-hierarchy"
+import { viewPullRequest } from "./view-pull-request"
 
 const allTools = {
   create_file: createFileTool,
@@ -32,7 +33,8 @@ const allTools = {
   post_github_comment: postGitHubCommentTool,
   update_pull_request: updatePullRequestTool,
   close_pull_request: closePullRequestTool,
-  list_pull_requests: listPullRequestsTool
+  list_pull_requests: listPullRequestsTool,
+  view_pull_request: viewPullRequest
 } as const;
 
 type ToolName = keyof typeof allTools;
