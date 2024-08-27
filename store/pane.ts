abc5b9bee845c2dc94a248f29f9d89b2fc30de44
Christopher David <chris@openagents.com>
2024-08-27 13:45:44 -0500

    pane lol

--- store/pane.ts
+++ store/pane.ts
@@ -48,7 +48,7 @@ export const usePaneStore = create<PaneStore>()(
       setActivePane: (id: string) => actions.setActivePane(set, id),
     }),
     {
-      name: 'openagents-hud-storage-1293761231233x34',
+      name: 'openagents-hud-storage-1293761231233x344',
       partialize: (state) => ({ panes: state.panes, lastPanePosition: state.lastPanePosition }),
     }
   )
