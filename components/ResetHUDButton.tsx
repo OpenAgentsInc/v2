c68ab599be440cfa7f6c90a073484407b1df84a5
Christopher David <chris@openagents.com>
2024-08-27 20:40:07 -0500

    Create file components/ResetHUDButton.tsx

---
+++ components/ResetHUDButton.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { usePaneStore } from '@/store/pane';
+
+const ResetHUDButton: React.FC = () => {
+  const resetHUDState = usePaneStore((state) => state.resetHUDState);
+
+  const handleReset = () => {
+    resetHUDState();
+  };
+
+  return (
+    <button
+      onClick={handleReset}
+      className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors"
+    >
+      Reset HUD
+    </button>
+  );
+};
+
+export default ResetHUDButton;
